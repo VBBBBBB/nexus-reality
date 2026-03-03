8f96aa404217b814bc742fbbe5742824d6c350cf
Vardhan Santosh Khinvasara <vardhansk18115@gmail.com>
2026-03-03 12:16:23 +0530

    Made it responsive , changed the theme to look more premium , add home in the navbar 2

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -3,10 +3,10 @@ import { GlowingEffect } from "../components/ui/glowing-effect";
 import { useState, useEffect } from "react";
 
 export default function About() {
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 1024);
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
     useEffect(() => {
-        const handleResize = () => setIsMobile(window.innerWidth < 1024);
+        const handleResize = () => setIsMobile(window.innerWidth < 768);
         window.addEventListener("resize", handleResize);
         return () => window.removeEventListener("resize", handleResize);
     }, []);
